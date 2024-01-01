653184bf61778ce918fab06bb3aa3c1f63ba1147
Kadir KARAKAS <kadirkarakas5546@gmail.com>
2024-01-01 13:48:27 +0300

    save

---
+++ .dart_tool/flutter_build/ed1d02f126a7da67012600269d47ea93/kernel_snapshot.d
@@ -0,0 +1,1 @@
+/Users/macbook/Documents/GitHub/Flutter100DayCamp/.dart_tool/flutter_build/ed1d02f126a7da67012600269d47ea93/app.dill: /Users/macbook/Documents/GitHub/Flutter100DayCamp/lib/Day1/print_kullanimi.dart
---
+++ lib/Day1/print_kullanimi.dart
@@ -0,0 +1,13 @@
+void main() {
+  print("Merhaba");
+
+  var ad = "Kadir";
+  int yas = 20;
+
+  print("$ad isimli kişini yaşı: $yas");
+
+  int sayi1 = 5;
+  int sayi2 = 10;
+
+  print("Girdiğiniz iki sayınınn toplamı ${sayi1 + sayi2}");
+}
