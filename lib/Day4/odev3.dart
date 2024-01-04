43f1b017e3c6e81fad8772c2e099cdfeba1e108a
Kadir KARAKAS <kadirkarakas5546@gmail.com>
2024-01-04 21:43:10 +0300

    save

---
+++ lib/Day4/odev3.dart
@@ -0,0 +1,18 @@
+class Odev3{
+  int maasHesapla(int gunSayisi){
+    int calismaSaati = gunSayisi*8;
+    print("Çalışma saati..:$calismaSaati");
+
+    int maas=0;
+
+    if(calismaSaati>160){
+      int mesaiFazlasi = calismaSaati-160;
+      maas = 160*10 + mesaiFazlasi+20;
+    }else{
+      maas = calismaSaati*10;
+    }
+
+    return maas;
+
+  }
+}
---
+++ lib/Day4/odev3_main.dart
@@ -0,0 +1,7 @@
+import 'package:fluttercamp100days/Day4/odev3.dart';
+
+void main(){
+  var odev3= Odev3();
+  int gelenMaas = odev3.maasHesapla(10);
+  print("Maaş: $gelenMaas");
+  }x
