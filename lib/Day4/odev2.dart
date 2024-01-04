0ea54f5aef9403eec2ea3dc03e0e5f1a9dfd4d45
Kadir KARAKAS <kadirkarakas5546@gmail.com>
2024-01-04 18:23:33 +0300

    save

---
+++ lib/Day4/odev2.dart
@@ -0,0 +1,9 @@
+class Odev2{
+
+  void hesapla(int kisaKenar , int uzunKenar){
+    int sonuc = 2*kisaKenar + 2*uzunKenar;
+    print(sonuc);
+  }
+
+
+}
---
+++ lib/Day4/odev2_main.dart
@@ -0,0 +1,7 @@
+import 'package:fluttercamp100days/Day4/odev2.dart';
+
+void main(){
+var hesapla = Odev2();
+  hesapla.hesapla(4, 2);
+
+}
