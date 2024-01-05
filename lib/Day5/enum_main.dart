86a7239c466fd592f74bdd24f202a04f34b000fc
Kadir KARAKAS <kadirkarakas5546@gmail.com>
2024-01-05 11:41:30 +0300

    save

---
+++ lib/Day5/enum_main.dart
@@ -0,0 +1,17 @@
+import 'package:fluttercamp100days/Day5/konserve_boyutu.dart';
+
+void main(){
+
+}
+
+void ucretAl(KonserveBoyut boyut){
+
+  switch(boyut){
+    case KonserveBoyut.Kucuk:{
+      print(20*30);
+    }
+    break;
+  }
+
+
+}
