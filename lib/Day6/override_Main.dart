c8dc8f8d3fcf99fbbcc6abd8c3abb728c4e28e30
Kadir KARAKAS <kadirkarakas5546@gmail.com>
2024-01-06 14:17:41 +0300

    save

---
+++ lib/Day6/override_Main.dart
@@ -0,0 +1,18 @@
+import 'package:fluttercamp100days/Day6/hayvan.dart';
+import 'package:fluttercamp100days/Day6/kedi.dart';
+import 'package:fluttercamp100days/Day6/kopek.dart';
+import 'package:fluttercamp100days/Day6/memeli.dart';
+
+void main(){
+  var hayvan=Hayvan();
+  hayvan.sesCikar();
+
+  var memeli=Memeli();
+  memeli.sesCikar();
+
+  var kedi=Kedi();
+  kedi.sesCikar();
+
+  var kopke=Kopek();
+  kopke.sesCikar();
+}
