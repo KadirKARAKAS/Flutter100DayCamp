643662a1610be5a2f9cf64678eff3c5f75236835
Kadir KARAKAS <kadirkarakas5546@gmail.com>
2024-01-19 20:20:39 +0300

    save

---
+++ lib/Day19/ciktialma.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class CiktiAlma extends StatefulWidget {
+  const CiktiAlma({super.key});
+
+  @override
+  State<CiktiAlma> createState() => _CiktiAlmaState();
+}
+
+class _CiktiAlmaState extends State<CiktiAlma> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold();
+  }
+}
